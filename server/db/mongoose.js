c8199c1381559f83064f7918cd9829be5e6776ec
rlam <rlam@egencia.com>
2018-05-11 23:01:02 +0200

    heroku mongo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://ds119930.mlab.com:19930/heroku_w5s095mq', {'useMongoClient': true});
+mongoose.connect(process.env.MONGOLAB_URI, {'useMongoClient': true});
 mongoose.Promise = global.Promise;
 
 module.exports = { mongoose };
