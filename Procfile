4c7277f1e0cc7366e036ac93f8ec483674048fca
rlam <rlam@egencia.com>
2018-05-12 09:07:37 +0200

    Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run stationsPopulate && npm run start
+web: npm run start
--- server/stationsPopulate.js
+++ server/stationsPopulate.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { mongoose } = require('./db/mongoose');
 const { Station } = require('./models/station');
 
-
+var counter = 0
 var linesList = ['M1', 'M2', 'M3', 'M3b', 'M4', 'M5', 'M6', 'M7', 'M7b', 'M8', 'M9', 'M10', 'M11', 'M12', 'M13', 'M14'];
 
 var db = mongoose.connection;
@@ -13,16 +13,21 @@ db.once('open', function() {
   var upsertStation = (updatedStationName, updateQuery) => {
     var findQuery = { name: updatedStationName };
     var properties = { upsert: true };
-    Station.findOneAndUpdate(findQuery, updateQuery, properties).catch((e) => console.log(e));
+    Station.findOneAndUpdate(findQuery, updateQuery, properties).then(() => {
+      counter++
+      console.log(counter)
+    }).catch((e) => console.log(e));
   };
   
   linesList.forEach((line) => {
+    console.log('Line', line)
     var data = fs.readFileSync('./stations/' + line + '.txt').toString();
     var dataSplit = data.split('\n');
     var previousStationName = null;
     var unidirection = false;
     dataSplit = dataSplit.filter((stationName) => (stationName));
     dataSplit.forEach(function(rowContent) {
+      console.log('Row content', rowContent)
       if(rowContent.includes('### Unidirection branch')) {
         previousStationName = rowContent.split("### Unidirection branch - linked to ").pop();
         unidirection = true;
